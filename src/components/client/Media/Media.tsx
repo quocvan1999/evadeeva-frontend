4e436fddaca8372141f22739a71f5fe358969eb6
QuocVan <lyquocvan1999@email.com>
2025-01-24 12:07:41 +0700

    media 2

--- src/components/client/Media/Media.tsx
+++ src/components/client/Media/Media.tsx
@@ -12,8 +12,8 @@ const Media = () => {
       <div className="bg-gray-100 py-6 text-2xl font-medium text-center">
         Follow us on media
       </div>
-      <div className="flex items-center justify-between">
-        <div className="w-[calc(100% / 4)] relative group">
+      <div className="flex w-full items-center justify-center">
+        <div className="w-[calc(100%/4)] relative group">
           <div className="absolute top-0 left-0 right-0 bottom-0 bg-gray-700 opacity-0 cursor-pointer transition-all duration-500 ease-in-out flex items-center justify-center group-hover:opacity-60"></div>
           <div className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center cursor-pointer opacity-0 group-hover:opacity-100 transition-all duration-500 ease-in-out">
             <div className=" w-20 h-20 bg-white opacity-100 flex items-center justify-center rounded-full">
@@ -31,7 +31,7 @@ const Media = () => {
           />
         </div>
 
-        <div className="w-[calc(100% / 4)] relative group">
+        <div className="w-[calc(100%/4)] relative group">
           <div className="absolute top-0 left-0 right-0 bottom-0 bg-gray-700 opacity-0 cursor-pointer transition-all duration-500 ease-in-out flex items-center justify-center group-hover:opacity-60"></div>
           <div className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center cursor-pointer opacity-0 group-hover:opacity-100 transition-all duration-500 ease-in-out">
             <div className=" w-20 h-20 bg-white opacity-100 flex items-center justify-center rounded-full">
@@ -49,7 +49,7 @@ const Media = () => {
           />
         </div>
 
-        <div className="w-[calc(100% / 4)] relative group">
+        <div className="w-[calc(100%/4)] relative group">
           <div className="absolute top-0 left-0 right-0 bottom-0 bg-gray-700 opacity-0 cursor-pointer transition-all duration-500 ease-in-out flex items-center justify-center group-hover:opacity-60"></div>
           <div className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center cursor-pointer opacity-0 group-hover:opacity-100 transition-all duration-500 ease-in-out">
             <div className=" w-20 h-20 bg-white opacity-100 flex items-center justify-center rounded-full">
@@ -67,7 +67,7 @@ const Media = () => {
           />
         </div>
 
-        <div className="w-[calc(100% / 4)] relative group">
+        <div className="w-[calc(100%/4)] relative group">
           <div className="absolute top-0 left-0 right-0 bottom-0 bg-gray-700 opacity-0 cursor-pointer transition-all duration-500 ease-in-out flex items-center justify-center group-hover:opacity-60"></div>
           <div className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center cursor-pointer opacity-0 group-hover:opacity-100 transition-all duration-500 ease-in-out">
             <div className=" w-20 h-20 bg-white opacity-100 flex items-center justify-center rounded-full">
